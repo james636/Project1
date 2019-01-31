6814796b6f4c9c422b446d3c0de0a70a3472c76e
Scott Sheppard <jssheppard83@gmail.com>
2019-01-31 14:02:56 -0600

    Created Hello World on node.js server with npm express middlware.

--- app.js
+++ app.js
@@ -1,54 +1,18 @@
-const http = require('http');
-const fs = require('fs')
-function rqListener(req, res) {
+const express = require('express');
 
-}
+const app = express();
 
-/*Can also create anonoymous function for server
-//http.createServer(function(req, res){
-
-}); event driven node uses heavily.
-http.createServer(rqListener);
-http.createServer((req, res)=>{
-});
-*/
-
-const server = http.createServer((req, res)=>{
-    const url = req.url;
-    const method = req.method;
-
-    if (url ==='/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head> <title> Enter Message</title><head>');
-        res.write('<body><form action="/message" method= "POST"><input type="text" name ="message"><button type ="submit"> Send</form></button></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url=== '/message' && method ==='POST'){
-        const body = [];
-        req.on('data', (chunk) =>{
-            console.log(chunk);
-            body.push(chunk);
-
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];fs.writeFileSync('message.txt', message);
-        });
-        
-        res.statusCode =302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    // process.exit(); hard exit event loop of server
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head> <title> My First Page</title><head>');
-    res.write('<body><h1>Hello World</h1></body>');
-    res.write('</html>');
-    res.end();
+app.use('/add-product', (req, res, next) => {
+    console.log('In 2 middleware!');
+    res.send('<h1>The "Please Add Product" Page</h1>'); 
 });
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('In 2 middleware!');
+    res.send('<h1>Hello World!</h1>');    //This last next dies or send response => res.send
+});
+
+app.listen(3000);
+
+
 
