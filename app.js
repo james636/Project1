436af4326e8b5624892ef40bbdb7659acb05cf89
Scott Sheppard <jssheppard83@gmail.com>
2019-02-16 01:39:03 -0600

    Aded View structure and shop structure.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const shopRoutes = require('./routes/shop.js');//importing routes, order doesn't
 
 app.use(express.static(path.join(__dirname, 'public')));//express.js feature serves files statically, read-access, needs the path import (up top)
 
-app.use(bodyParser.urlencoded({extended: true})); //function registers middleware function, package calls next so reqest reachers middleware. Won't parse files, need different parser
+app.use(bodyParser.urlencoded({extended: false})); //function registers middleware function, package calls next so reqest reachers middleware. Won't parse files, need different parser
 
 app.use('/admin', adminRoutes);//admin routes handler, order matters
 app.use(shopRoutes);//order matters because get here
